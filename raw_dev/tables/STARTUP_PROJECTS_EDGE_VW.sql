df8d638a3893660b6544b10388fc247871b84e83
vthotausgbc <vthota@usgbc.org>
2024-02-25 12:23:45 -0500

    drop view from table folder

--- raw_dev/tables/STARTUP_PROJECTS_EDGE_VW.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset  20240218:STARTUP_PROJECTS_EDGE_VW  
-
-use schema API;
-create or replace view STARTUP_PROJECTS_EDGE_VW as 
-select * from 
-RAW_DEV.api.STARTUP_PROJECTS_EDGE1;
