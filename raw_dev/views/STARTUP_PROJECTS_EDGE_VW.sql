f2adb836c974c805fd0a4066d8948694b4f6b13d
vthotausgbc <vthota@usgbc.org>
2024-02-25 12:20:31 -0500

    create view

---
+++ raw_dev/views/STARTUP_PROJECTS_EDGE_VW.sql
@@ -0,0 +1,8 @@
+--liquibase formatted sql
+
+--changeset  20240219:STARTUP_PROJECTS_EDGE_VW  
+
+use schema API;
+create or replace view STARTUP_PROJECTS_EDGE_VW as 
+select * from 
+RAW_DEV.api.STARTUP_PROJECTS_EDGE1;
